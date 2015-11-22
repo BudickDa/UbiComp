3126350295f177757e98c81355c3406e6da3ded0
budickda <daniel@budick.eu>
2015-11-22 20:10:19 +0100

    ...

--- server/startup.js
+++ server/startup.js
@@ -1,11 +1,11 @@
 Data.remove({});
 Data.insert({type: 'color', value: 'blue'});
 
-var chance = new Chance(), temperature = 23.00;
+var chance = new Chance(), temperature = 21.00;
 Meteor.startup(function () {
     Data.insert({type: 'temperature', value: temperature});
     Meteor.setInterval(function () {
-        temperature += chance.floating({min: -1, max: 1});
+        temperature += chance.floating({min: -0.015, max: 0.015});
         Data.update({type: 'temperature'}, {$set: {value: temperature}});
     }, 1000);
 });
